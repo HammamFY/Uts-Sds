5275f629b66d6bbd6e1e4dc07f36d06d67ba02be
HammamFY <145745926+HammamFY@users.noreply.github.com>
2023-10-07 03:11:24 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -11,6 +11,16 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	
-	DB.AutoMigrate(models.User{} )
+	dsn := "root@tcp(127.0.0.1:3306)/admin_management_user?charset=utf8mb4&parseTime=True&loc=Local"
+	conn, err:= gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+	  fmt.Println("database tidak dapat terhubung")
+	}
+	
+   fmt.Println("Database terkoneksi")
+  
+   DB = conn
+   DB.AutoMigrate(models.User{}, models.Admin{})
 }
